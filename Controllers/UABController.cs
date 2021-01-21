6748497ab5576faf84002af45c8982e4d368ce02
Venkat <venkateswarao.mokka@anzamedicode.com>
2021-01-21 17:24:13 +0530

    Added views.

---
+++ Controllers/UABController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UAB.Controllers
+{
+    public class UABController : Controller
+    {
+        public IActionResult Coding()
+        {
+            return View();
+        }
+        public IActionResult GlobalAuditing()
+        {
+            return View();
+        }
+
+        public IActionResult DomesticAuditing()
+        {
+            return View();
+        }
+
+    }
+}
