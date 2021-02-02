c5fc7d1fba593ad0bc78680bdc98899271a5d6ef
Venkat <venkateswarao.mokka@anzamedicode.com>
2021-02-02 19:43:56 +0530

    Added the code to load the dashboard dynamically.

--- Controllers/UABController.cs
+++ Controllers/UABController.cs
@@ -3,18 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using UAB.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace UAB.Controllers
 {
     public class UABController : Controller
     {
+        UAB.DAL.Models.UABContext _dbcontext = null;
         public IActionResult CodingSummary()
         {
-            return View();
+            List<UAB.Models.UABDashboardDetails> lstworkitem = new List<UAB.Models.UABDashboardDetails>();
+
+            lstworkitem.Add(new UABDashboardDetails() { ProjectName = "Phase1 - Ambulatory", AvailableCount = 1, IncorrectCount = 0, ApprovedCount = 0 });
+            lstworkitem.Add(new UABDashboardDetails() { ProjectName = "Phase1 - IP", AvailableCount = 0, IncorrectCount = 0, ApprovedCount = 0 });
+            lstworkitem.Add(new UABDashboardDetails() { ProjectName = "Phase2 - Pulmonology", AvailableCount = 0, IncorrectCount = 0, ApprovedCount = 0 });
+
+
+
+            //using (_dbcontext = new DAL.Models.UABContext())
+            //{
+
+            //lstworkitem = (from cc in _dbcontext.ClinicalCase
+            //               select new UABDashboardDetails()
+            //               {
+            //                   ProjectName = cc.Project.Name,
+            //                   AvailableCount = cc.WorkItem.Select(a => a.StatusId == 1).Count(),
+            //                   IncorrectCount = cc.WorkItem.Select(a => a.StatusId == 2).Count(),
+            //                   ApprovedCount = cc.WorkItem.Select(a => a.StatusId == 3).Count()
+            //               }).ToList();
+
+            //var qry = (from pr in _dbcontext.Project
+            //           join cc in _dbcontext.ClinicalCase on pr.ProjectId equals cc.ProjectId
+            //           into jrs
+            //           from jrResult in jrs.DefaultIfEmpty()
+            //           join w in _dbcontext.WorkItem on jrResult.ClinicalCaseId equals w.ClinicalCaseId
+            //            into jr
+            //           from jrresult in jrs.DefaultIfEmpty().Select(
+            //           new UABDashboardDetails()
+            //           (
+            //               ProjectName = pr.Name,
+            //               AvailableCount = jrresult.WorkItem.Select(a => a.StatusId == 1).Count(),
+            //               IncorrectCount = jrresult.WorkItem.Select(a => a.StatusId == 2).Count(),
+            //               ApprovedCount = jrresult.WorkItem.Select(a => a.StatusId == 3).Count()
+
+            //           )).ToList();
+
+            //}
+            return View(lstworkitem);
+        }
+
+        private object UABDashboardDetails()
+        {
+            throw new NotImplementedException();
         }
 
         public IActionResult Coding()
         {
+            using (_dbcontext = new DAL.Models.UABContext())
+            {
+
+            }
             return View();
         }
 
--- Models/UAB.cs
+++ Models/UAB.cs
@@ -5,8 +5,12 @@ using System.Threading.Tasks;
 
 namespace UAB.Models
 {
-    public class UAB1
+    public class UABDashboardDetails
     {
+        public string ProjectName { get; set; }
+        public int AvailableCount { get; set; }
+        public int IncorrectCount { get; set; }
+        public int ApprovedCount { get; set; }
     }
     public class QADetails
     {
