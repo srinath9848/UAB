175441a0b0365c1d8ad61f1d886ba8229a5093ab
srinath <srinath.banda@aviacode.com>
2021-02-09 10:29:01 +0530

    Committed the code for QA fetching and submission

--- UAB.DTO/ChartSummaryDTO.cs
+++ UAB.DTO/ChartSummaryDTO.cs
@@ -16,18 +16,33 @@ namespace UAB.DTO
         public int ProviderFeedbackID { get; set; }
         public int AssignedTo { get; set; }
         [Required(ErrorMessage = "Payor is required.")]
-        public string PayorID { get; set; }
+        public int PayorID { get; set; }
+
         [Required(ErrorMessage = "Provider is required.")]
-        public string ProviderID { get; set; }
+        public int ProviderID { get; set; }
         [Required(ErrorMessage = "CPT Code is required.")]
         public string CPTCode { get; set; }
+
         public string NoteTitle { get; set; }
         [Required(ErrorMessage = "Dx Code is required.")]
         public string Dx { get; set; }
+
         [Required(ErrorMessage = "Modifiers is required.")]
         public string Mod { get; set; }
         public string CoderQuestion { get; set; }
         public CodingDTO CodingDTO { get; set; }
         public QADTO QADTO { get; set; }
+        public int QAPayorID { get; set; }
+        public string QAPayorRemarks { get; set; }
+        public int QAProviderID { get; set; }
+        public string QAProviderRemarks { get; set; }
+        public string QAMod { get; set; }
+        public string QAModRemarks { get; set; }
+        public string QADx { get; set; }
+        public string QADxRemarks { get; set; }
+        public string QACPTCode { get; set; }
+        public string QACPTCodeRemarks { get; set; }
+        public int QAProviderFeedbackID { get; set; }
+        public string QAProviderFeedbackRemarks { get; set; }        
     }
 }
