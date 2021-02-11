5063c227fd6daf614b3a8f0b7faaa6ff6a6b4ae8
naresh.darbar <naresh.darbar@AZMLAP026>
2021-02-11 11:33:20 +0530

    PC-3755 Error message For Inavlid Login Credentails

--- UAB.Web/Controllers/AccountController.cs
+++ UAB.Web/Controllers/AccountController.cs
@@ -39,7 +39,9 @@ namespace UAB.Controllers
         [HttpPost]
         public IActionResult Login([FromForm] LoginViewModel model)
         {
-           
+            try
+            {
+
                 if (ModelState.IsValid)
                 {
                     if (!string.IsNullOrEmpty(model.Email) && string.IsNullOrEmpty(model.Password))
@@ -50,12 +52,18 @@ namespace UAB.Controllers
                     if (signInResult.Result != 0)
                     {
                         Auth.isAuth = false;
-                        throw new ApplicationException("INVALID_LOGIN_ATTEMPT");
+                        throw new ApplicationException("Invalid credentails");
+                        
                     }
                     Auth.isAuth = true;
                     Auth.CurrentUserName = model.Email;
                     return RedirectToAction("Index", "Home");
                 }
+            }
+            catch (Exception ex)
+            {
+                TempData["LoginFailed"] = ex.Message;
+            }
             return View();
             
         }
