4a5ba1e0359487df255b7a41634aaa4639f7d7f5
naresh.darbar <naresh.darbar@AZMLAP026>
2021-02-17 15:05:00 +0530

    added validations for models.

--- UAB.DAL/Models/Payor.cs
+++ UAB.DAL/Models/Payor.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
 
 namespace UAB.DAL.Models
 {
     public partial class Payor
     {
         public int PayorId { get; set; }
+        [Required(ErrorMessage = "Payor is required.")]
+        [RegularExpression(@"^(([A-za-z0-9]+[\s]{1}[A-za-z0-9]+)|([A-Za-z0-9]+))$", ErrorMessage = "Special Character not allowed. Please check and remove special character from Payor")]
         public string Name { get; set; }
     }
 }
--- UAB.DAL/Models/Provider.cs
+++ UAB.DAL/Models/Provider.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.ComponentModel.DataAnnotations;
 namespace UAB.DAL.Models
 {
     public partial class Provider
     {
         public int ProviderId { get; set; }
+        [Required(ErrorMessage = "Provider is required.")]
+        [RegularExpression(@"^(([A-za-z0-9]+[\s]{1}[A-za-z0-9]+)|([A-Za-z0-9]+))$", ErrorMessage = "Special Character not allowed")]
         public string Name { get; set; }
     }
 }
