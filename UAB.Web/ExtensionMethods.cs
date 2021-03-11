9af5189849edcde54bcee0e233d7d1af000e61a7
srinath <srinath.banda@aviacode.com>
2021-03-11 17:58:01 +0530

    Committed the code to get the time based on time zone

---
+++ UAB.Web/ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UAB
+{
+    public static class ExtensionMethods
+    {
+        public static DateTime ToLocalDate(this DateTime dateTime)
+        {
+            DateTime dt = TimeZoneInfo.ConvertTimeToUtc(dateTime, TimeZoneInfo.FindSystemTimeZoneById("Mountain Standard Time"));
+            return TimeZoneInfo.ConvertTimeFromUtc(dt, TimeZoneInfo.Local);
+        }
+    }
+}
