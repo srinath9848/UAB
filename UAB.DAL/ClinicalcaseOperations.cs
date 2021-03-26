22cebcb24156874da2a13e4db05f59ddb6bed459
Venkat <venkateswarao.mokka@anzamedicode.com>
2021-03-26 19:16:53 +0530

    PC-4075 :- Fetching CPT Codes in Incorrect Charts.

--- UAB.DAL/ClinicalcaseOperations.cs
+++ UAB.DAL/ClinicalcaseOperations.cs
@@ -346,7 +346,7 @@ namespace UAB.DAL
                             chartSummaryDTO.CPTCode = Convert.ToString(reader["CPTCode"]);
                             chartSummaryDTO.QACPTCode = Convert.ToString(reader["QACPTCode"]);
                             chartSummaryDTO.QACPTCodeRemarks = Convert.ToString(reader["QACPTCodeRemark"]);
-                            chartSummaryDTO.Mod = Convert.ToString(reader["Modifier"]);
+                            //chartSummaryDTO.Mod = Convert.ToString(reader["Modifier"]);
                             chartSummaryDTO.QAMod = Convert.ToString(reader["QAMod"]);
                             chartSummaryDTO.QAModRemarks = Convert.ToString(reader["QAModRemark"]);
                             if (reader["ProviderFeedbackID"] != DBNull.Value)
