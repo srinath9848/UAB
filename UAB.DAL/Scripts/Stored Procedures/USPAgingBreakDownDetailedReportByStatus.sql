8ecf8f20c59ae8c20a5eb5570518c66e797b3226
Rahul Kumar Banergee <rahulkumar.banerjee@aviacode.com>
2021-06-18 15:04:45 +0530

    UAB-2019 : Fixed duplicate data issue for Aging break down and Chart Summary report.

--- UAB.DAL/Scripts/Stored Procedures/USPAgingBreakDownDetailedReportByStatus.sql
+++ UAB.DAL/Scripts/Stored Procedures/USPAgingBreakDownDetailedReportByStatus.sql
@@ -1,220 +1,235 @@
-CREATE PROCEDURE [dbo].[USPAgingBreakDownDetailedReportByStatus] (@ProjectId INT,@ProjectTypeName varchar(50),@ColumnName varchar(50))
-AS
-BEGIN
-IF @ColumnName = 'Due'
-BEGIN
-IF @ProjectTypeName = 'Ambulatory'
-BEGIN
-SELECT CC.DateOfService AS DateOfService,L.Name AS ListName,CC.ClinicalCaseId AS ClinicalCaseId
- ,CC.PatientMRN AS PatientMRN, CC.PatientFirstName+' '+CC.PatientLastName AS Name,Pro.Name AS Provider
-FROM Project p
-JOIN ProjectType pt
-ON p.ProjectTypeId = pt.ProjectTypeId
-JOIN WorkItem wi
-ON wi.ProjectId = p.ProjectId
-JOIN ClinicalCase cc
-ON wi.ClinicalCaseId = cc.ClinicalCaseId
-LEFT JOIN List L ON L.ListId = CC.ListId
- LEFT JOIN Provider Pro ON Pro.ProviderId = CC.ProviderId
-WHERE wi.StatusId < 16 --only not posted items
-AND pt.ProjectTypeName = @ProjectTypeName
-AND p.ProjectId = @ProjectId
-AND DATEDIFF(DAY, cc.DateOfService, GETDATE()) > 3
-END
-ELSE IF @ProjectTypeName = 'IP'
-BEGIN
-SELECT CC.DateOfService AS DateOfService,L.Name AS ListName,CC.ClinicalCaseId AS ClinicalCaseId
- ,CC.PatientMRN AS PatientMRN, CC.PatientFirstName+' '+CC.PatientLastName AS Name,Pro.Name AS Provider
-FROM Project p
-JOIN ProjectType pt
-ON p.ProjectTypeId = pt.ProjectTypeId
-JOIN WorkItem wi
-ON wi.ProjectId = p.ProjectId
-JOIN ClinicalCase cc
-ON wi.ClinicalCaseId = cc.ClinicalCaseId
-LEFT JOIN List L ON L.ListId = CC.ListId
- LEFT JOIN Provider Pro ON Pro.ProviderId = CC.ProviderId
-WHERE wi.StatusId < 16 --only not posted items
-AND pt.ProjectTypeName = @ProjectTypeName
-AND p.ProjectId = @ProjectId
-AND DATEDIFF(DAY, cc.DateOfService, GETDATE()) > 5
-END
-END
-ELSE IF @ColumnName = 'Total'
-BEGIN
-SELECT CC.DateOfService AS DateOfService,L.Name AS ListName,CC.ClinicalCaseId AS ClinicalCaseId
- ,CC.PatientMRN AS PatientMRN, CC.PatientFirstName+' '+CC.PatientLastName AS Name,Pro.Name AS Provider
-FROM Project p
-JOIN ProjectType pt
-ON p.ProjectTypeId = pt.ProjectTypeId
-JOIN WorkItem wi
-ON wi.ProjectId = p.ProjectId
-JOIN ClinicalCase cc
-ON wi.ClinicalCaseId = cc.ClinicalCaseId
-LEFT JOIN List L ON L.ListId = CC.ListId
- LEFT JOIN Provider Pro ON Pro.ProviderId = CC.ProviderId
-WHERE wi.StatusId < 16 --only not posted items
-AND pt.ProjectTypeName = @ProjectTypeName
-AND p.ProjectId = @ProjectId
---AND DATEDIFF(DAY, cc.DateOfService, GETDATE()) BETWEEN 0 AND 3
-AND (DATEDIFF(DAY, cc.DateOfService, GETDATE()) > 3 OR DATEDIFF(DAY, cc.DateOfService, GETDATE()) BETWEEN 0 AND 3 OR DATEDIFF(DAY, cc.DateOfService, GETDATE()) BETWEEN 4 AND 5 
-OR DATEDIFF(DAY, cc.DateOfService, GETDATE()) BETWEEN 6 AND 7 OR DATEDIFF(DAY, cc.DateOfService, GETDATE()) > 7)
-END
-ELSE IF @ColumnName = 'Not Ready For Coding'
-BEGIN
-IF @ProjectTypeName = 'Ambulatory'
-BEGIN
-SELECT CC.DateOfService AS DateOfService,L.Name AS ListName,CC.ClinicalCaseId AS ClinicalCaseId
- ,CC.PatientMRN AS PatientMRN, CC.PatientFirstName+' '+CC.PatientLastName AS Name,Pro.Name AS Provider
-FROM Project p
-JOIN ProjectType pt
-ON p.ProjectTypeId = pt.ProjectTypeId
-JOIN WorkItem wi
-ON wi.ProjectId = p.ProjectId
-JOIN ClinicalCase cc
-ON wi.ClinicalCaseId = cc.ClinicalCaseId
-LEFT JOIN List L ON L.ListId = CC.ListId
- LEFT JOIN Provider Pro ON Pro.ProviderId = CC.ProviderId
-WHERE wi.StatusId < 16 --only not posted items
-AND pt.ProjectTypeName = @ProjectTypeName
-AND p.ProjectId=@ProjectId
-AND wi.StatusId = 1 
-AND (pt.ProjectTypeName = @ProjectTypeName AND DATEDIFF(DAY, cc.DateOfService, GETDATE()) <= 3 )
-END
-ELSE IF @ProjectTypeName = 'IP'
-BEGIN
-SELECT CC.DateOfService AS DateOfService,L.Name AS ListName,CC.ClinicalCaseId AS ClinicalCaseId
- ,CC.PatientMRN AS PatientMRN, CC.PatientFirstName+' '+CC.PatientLastName AS Name,Pro.Name AS Provider
-FROM Project p
-JOIN ProjectType pt
-ON p.ProjectTypeId = pt.ProjectTypeId
-JOIN WorkItem wi
-ON wi.ProjectId = p.ProjectId
-JOIN ClinicalCase cc
-ON wi.ClinicalCaseId = cc.ClinicalCaseId
-LEFT JOIN List L ON L.ListId = CC.ListId
- LEFT JOIN Provider Pro ON Pro.ProviderId = CC.ProviderId
-WHERE wi.StatusId < 16 --only not posted items
-AND pt.ProjectTypeName = @ProjectTypeName
-AND p.ProjectId=@ProjectId
-AND wi.StatusId = 1 
-AND (pt.ProjectTypeName = @ProjectTypeName AND DATEDIFF(DAY, cc.DateOfService, GETDATE()) <= 5 )
-END
-END
-ELSE IF @ColumnName = 'Ready For Coding'
-BEGIN
-IF @ProjectTypeName = 'Ambulatory'
-BEGIN
-SELECT CC.DateOfService AS DateOfService,L.Name AS ListName,CC.ClinicalCaseId AS ClinicalCaseId
- ,CC.PatientMRN AS PatientMRN, CC.PatientFirstName+' '+CC.PatientLastName AS Name,Pro.Name AS Provider
-FROM Project p
-JOIN ProjectType pt
-ON p.ProjectTypeId = pt.ProjectTypeId
-JOIN WorkItem wi
-ON wi.ProjectId = p.ProjectId
-JOIN ClinicalCase cc
-ON wi.ClinicalCaseId = cc.ClinicalCaseId
-LEFT JOIN List L ON L.ListId = CC.ListId
- LEFT JOIN Provider Pro ON Pro.ProviderId = CC.ProviderId
-WHERE wi.StatusId < 16 --only not posted items
-AND pt.ProjectTypeName = @ProjectTypeName
-AND p.ProjectId=@ProjectId
-AND wi.StatusId = 1 
-AND (pt.ProjectTypeName = @ProjectTypeName AND DATEDIFF(DAY, cc.DateOfService, GETDATE()) > 3 )
-END
-ELSE IF @ProjectTypeName = 'IP'
-BEGIN
-SELECT CC.DateOfService AS DateOfService,L.Name AS ListName,CC.ClinicalCaseId AS ClinicalCaseId
- ,CC.PatientMRN AS PatientMRN, CC.PatientFirstName+' '+CC.PatientLastName AS Name,Pro.Name AS Provider
-FROM Project p
-JOIN ProjectType pt
-ON p.ProjectTypeId = pt.ProjectTypeId
-JOIN WorkItem wi
-ON wi.ProjectId = p.ProjectId
-JOIN ClinicalCase cc
-ON wi.ClinicalCaseId = cc.ClinicalCaseId
-LEFT JOIN List L ON L.ListId = CC.ListId
- LEFT JOIN Provider Pro ON Pro.ProviderId = CC.ProviderId
-WHERE wi.StatusId < 16 --only not posted items
-AND pt.ProjectTypeName = @ProjectTypeName
-AND p.ProjectId=@ProjectId
-AND wi.StatusId = 1 
-AND (pt.ProjectTypeName = @ProjectTypeName AND DATEDIFF(DAY, cc.DateOfService, GETDATE()) > 5 )
-END
-END
-ELSE IF @ColumnName = 'In Coding'
-BEGIN
-SELECT CC.DateOfService AS DateOfService,L.Name AS ListName,CC.ClinicalCaseId AS ClinicalCaseId
- ,CC.PatientMRN AS PatientMRN, CC.PatientFirstName+' '+CC.PatientLastName AS Name,Pro.Name AS Provider
-FROM Project p
-JOIN ProjectType pt
-ON p.ProjectTypeId = pt.ProjectTypeId
-JOIN WorkItem wi
-ON wi.ProjectId = p.ProjectId
-JOIN ClinicalCase cc
-ON wi.ClinicalCaseId = cc.ClinicalCaseId
-LEFT JOIN List L ON L.ListId = CC.ListId
- LEFT JOIN Provider Pro ON Pro.ProviderId = CC.ProviderId
-WHERE wi.StatusId < 16 --only not posted items
-AND pt.ProjectTypeName = @ProjectTypeName
-AND p.ProjectId=@ProjectId
-AND wi.StatusId IN (2, 14) AND wi.isBlocked = 0
-END
-ELSE IF @ColumnName = 'In QA'
-BEGIN
-SELECT CC.DateOfService AS DateOfService,L.Name AS ListName,CC.ClinicalCaseId AS ClinicalCaseId
- ,CC.PatientMRN AS PatientMRN, CC.PatientFirstName+' '+CC.PatientLastName AS Name,Pro.Name AS Provider
-FROM Project p
-JOIN ProjectType pt
-ON p.ProjectTypeId = pt.ProjectTypeId
-JOIN WorkItem wi
-ON wi.ProjectId = p.ProjectId
-JOIN ClinicalCase cc
-ON wi.ClinicalCaseId = cc.ClinicalCaseId
-LEFT JOIN List L ON L.ListId = CC.ListId
- LEFT JOIN Provider Pro ON Pro.ProviderId = CC.ProviderId
-WHERE wi.StatusId < 16 --only not posted items
-AND pt.ProjectTypeName = @ProjectTypeName
-AND p.ProjectId=@ProjectId
-AND wi.StatusId IN (4, 5, 12) AND wi.isBlocked = 0
-END
-ELSE IF @ColumnName = 'In ShadowQA'
-BEGIN
-SELECT CC.DateOfService AS DateOfService,L.Name AS ListName,CC.ClinicalCaseId AS ClinicalCaseId
- ,CC.PatientMRN AS PatientMRN, CC.PatientFirstName+' '+CC.PatientLastName AS Name,Pro.Name AS Provider
-FROM Project p
-JOIN ProjectType pt
-ON p.ProjectTypeId = pt.ProjectTypeId
-JOIN WorkItem wi
-ON wi.ProjectId = p.ProjectId
-JOIN ClinicalCase cc
-ON wi.ClinicalCaseId = cc.ClinicalCaseId
-LEFT JOIN List L ON L.ListId = CC.ListId
- LEFT JOIN Provider Pro ON Pro.ProviderId = CC.ProviderId
-WHERE wi.StatusId < 16 --only not posted items
-AND pt.ProjectTypeName = @ProjectTypeName
-AND p.ProjectId=@ProjectId
-AND wi.StatusId IN (8, 9, 13) AND wi.isBlocked = 0
-END
-ELSE IF @ColumnName = 'Ready For Posting'
-BEGIN
-SELECT CC.DateOfService AS DateOfService,L.Name AS ListName,CC.ClinicalCaseId AS ClinicalCaseId
- ,CC.PatientMRN AS PatientMRN, CC.PatientFirstName+' '+CC.PatientLastName AS Name,Pro.Name AS Provider
-FROM Project p
-JOIN ProjectType pt
-ON p.ProjectTypeId = pt.ProjectTypeId
-JOIN WorkItem wi
-ON wi.ProjectId = p.ProjectId
-JOIN ClinicalCase cc
-ON wi.ClinicalCaseId = cc.ClinicalCaseId
-LEFT JOIN List L ON L.ListId = CC.ListId
- LEFT JOIN Provider Pro ON Pro.ProviderId = CC.ProviderId
-WHERE wi.StatusId < 16 --only not posted items
-AND pt.ProjectTypeName = @ProjectTypeName
-AND p.ProjectId=@ProjectId
-AND wi.StatusId = 15 AND wi.isBlocked = 0
-END
-END
+CREATE PROCEDURE [dbo].[USPAgingBreakDownDetailedReportByStatus] (@ProjectId INT,@ProjectTypeName varchar(50),@ColumnName varchar(50))    
+AS    
+BEGIN    
+IF @ColumnName = 'Due'    
+BEGIN    
+IF @ProjectTypeName = 'Ambulatory'    
+BEGIN    
+SELECT distinct CC.DateOfService AS DateOfService,L.Name AS ListName,CC.ClinicalCaseId AS ClinicalCaseId    
+ ,CC.PatientMRN AS PatientMRN, CC.PatientFirstName+' '+CC.PatientLastName AS Name,Pro.Name AS Provider    
+FROM Project p    
+JOIN ProjectType pt    
+ON p.ProjectTypeId = pt.ProjectTypeId    
+JOIN WorkItem wi    
+ON wi.ProjectId = p.ProjectId    
+JOIN ClinicalCase cc    
+ON wi.ClinicalCaseId = cc.ClinicalCaseId    
+LEFT JOIN List L ON L.ListId = CC.ListId    
+ LEFT JOIN Provider Pro ON Pro.ProviderId = CC.ProviderId    
+WHERE wi.StatusId < 16 --only not posted items    
+AND pt.ProjectTypeName = @ProjectTypeName    
+AND p.ProjectId = @ProjectId    
+AND DATEDIFF(DAY, cc.DateOfService, GETUTCDATE()) > 3    
+END    
+ELSE IF @ProjectTypeName = 'IP'    
+BEGIN    
+SELECT distinct CC.DateOfService AS DateOfService,L.Name AS ListName,CC.ClinicalCaseId AS ClinicalCaseId    
+ ,CC.PatientMRN AS PatientMRN, CC.PatientFirstName+' '+CC.PatientLastName AS Name,  ISNULL(Pro.Name,PP.Name) AS Provider      
+FROM Project p    
+JOIN ProjectType pt    
+ON p.ProjectTypeId = pt.ProjectTypeId    
+JOIN WorkItem wi    
+ON wi.ProjectId = p.ProjectId    
+JOIN ClinicalCase cc    
+ON wi.ClinicalCaseId = cc.ClinicalCaseId    
+LEFT JOIN List L ON L.ListId = CC.ListId    
+ LEFT JOIN Provider Pro ON Pro.ProviderId = CC.ProviderId    
+  LEFT JOIN WorkItemProvider Wip ON Wip.ClinicalCaseId = CC.ClinicalCaseId
+ LEFT JOIN Provider PP ON PP.ProviderID = Wip.ProviderId
+WHERE wi.StatusId < 16 --only not posted items    
+AND pt.ProjectTypeName = @ProjectTypeName    
+AND p.ProjectId = @ProjectId    
+AND DATEDIFF(DAY, cc.DateOfService, GETUTCDATE()) > 5    
+END    
+END    
+ELSE IF @ColumnName = 'Total'    
+BEGIN    
+SELECT distinct CC.DateOfService AS DateOfService,L.Name AS ListName,CC.ClinicalCaseId AS ClinicalCaseId    
+ ,CC.PatientMRN AS PatientMRN, CC.PatientFirstName+' '+CC.PatientLastName AS Name,  ISNULL(Pro.Name,PP.Name) AS Provider       
+FROM Project p    
+JOIN ProjectType pt    
+ON p.ProjectTypeId = pt.ProjectTypeId    
+JOIN WorkItem wi    
+ON wi.ProjectId = p.ProjectId    
+JOIN ClinicalCase cc    
+ON wi.ClinicalCaseId = cc.ClinicalCaseId    
+LEFT JOIN List L ON L.ListId = CC.ListId    
+ LEFT JOIN Provider Pro ON Pro.ProviderId = CC.ProviderId    
+  LEFT JOIN WorkItemProvider Wip ON Wip.ClinicalCaseId = CC.ClinicalCaseId
+ LEFT JOIN Provider PP ON PP.ProviderID = Wip.ProviderId
+WHERE wi.StatusId < 16 --only not posted items    
+AND pt.ProjectTypeName = @ProjectTypeName    
+AND p.ProjectId = @ProjectId    
+--AND DATEDIFF(DAY, cc.DateOfService, GETUTCDATE()) BETWEEN 0 AND 3    
+AND (DATEDIFF(DAY, cc.DateOfService, GETUTCDATE()) > 3 OR DATEDIFF(DAY, cc.DateOfService, GETUTCDATE()) BETWEEN 0 AND 3 OR DATEDIFF(DAY, cc.DateOfService, GETUTCDATE()) BETWEEN 4 AND 5     
+OR DATEDIFF(DAY, cc.DateOfService, GETUTCDATE()) BETWEEN 6 AND 7 OR DATEDIFF(DAY, cc.DateOfService, GETUTCDATE()) > 7)    
+END    
+ELSE IF @ColumnName = 'Not Ready For Coding'    
+BEGIN    
+IF @ProjectTypeName = 'Ambulatory'    
+BEGIN    
+SELECT distinct CC.DateOfService AS DateOfService,L.Name AS ListName,CC.ClinicalCaseId AS ClinicalCaseId    
+ ,CC.PatientMRN AS PatientMRN, CC.PatientFirstName+' '+CC.PatientLastName AS Name,Pro.Name AS Provider    
+FROM Project p    
+JOIN ProjectType pt    
+ON p.ProjectTypeId = pt.ProjectTypeId    
+JOIN WorkItem wi    
+ON wi.ProjectId = p.ProjectId    
+JOIN ClinicalCase cc    
+ON wi.ClinicalCaseId = cc.ClinicalCaseId    
+LEFT JOIN List L ON L.ListId = CC.ListId    
+ LEFT JOIN Provider Pro ON Pro.ProviderId = CC.ProviderId    
+WHERE wi.StatusId < 16 --only not posted items    
+AND pt.ProjectTypeName = @ProjectTypeName    
+AND p.ProjectId=@ProjectId    
+AND wi.StatusId = 1     
+AND (pt.ProjectTypeName = @ProjectTypeName AND DATEDIFF(DAY, cc.DateOfService, GETUTCDATE()) <= 3 )    
+END    
+ELSE IF @ProjectTypeName = 'IP'    
+BEGIN    
+SELECT distinct CC.DateOfService AS DateOfService,L.Name AS ListName,CC.ClinicalCaseId AS ClinicalCaseId    
+ ,CC.PatientMRN AS PatientMRN, CC.PatientFirstName+' '+CC.PatientLastName AS Name,  ISNULL(Pro.Name,PP.Name) AS Provider      
+FROM Project p    
+JOIN ProjectType pt    
+ON p.ProjectTypeId = pt.ProjectTypeId    
+JOIN WorkItem wi    
+ON wi.ProjectId = p.ProjectId    
+JOIN ClinicalCase cc    
+ON wi.ClinicalCaseId = cc.ClinicalCaseId    
+LEFT JOIN List L ON L.ListId = CC.ListId    
+ LEFT JOIN Provider Pro ON Pro.ProviderId = CC.ProviderId    
+  LEFT JOIN WorkItemProvider Wip ON Wip.ClinicalCaseId = CC.ClinicalCaseId
+ LEFT JOIN Provider PP ON PP.ProviderID = Wip.ProviderId
+WHERE wi.StatusId < 16 --only not posted items    
+AND pt.ProjectTypeName = @ProjectTypeName    
+AND p.ProjectId=@ProjectId    
+AND wi.StatusId = 1     
+AND (pt.ProjectTypeName = @ProjectTypeName AND DATEDIFF(DAY, cc.DateOfService, GETUTCDATE()) <= 5 )    
+END    
+END    
+ELSE IF @ColumnName = 'Ready For Coding'    
+BEGIN    
+IF @ProjectTypeName = 'Ambulatory'    
+BEGIN    
+SELECT distinct CC.DateOfService AS DateOfService,L.Name AS ListName,CC.ClinicalCaseId AS ClinicalCaseId    
+ ,CC.PatientMRN AS PatientMRN, CC.PatientFirstName+' '+CC.PatientLastName AS Name,Pro.Name AS Provider    
+FROM Project p    
+JOIN ProjectType pt    
+ON p.ProjectTypeId = pt.ProjectTypeId    
+JOIN WorkItem wi    
+ON wi.ProjectId = p.ProjectId    
+JOIN ClinicalCase cc    
+ON wi.ClinicalCaseId = cc.ClinicalCaseId    
+LEFT JOIN List L ON L.ListId = CC.ListId    
+ LEFT JOIN Provider Pro ON Pro.ProviderId = CC.ProviderId    
+WHERE wi.StatusId < 16 --only not posted items    
+AND pt.ProjectTypeName = @ProjectTypeName    
+AND p.ProjectId=@ProjectId    
+AND wi.StatusId = 1     
+AND (pt.ProjectTypeName = @ProjectTypeName AND DATEDIFF(DAY, cc.DateOfService, GETUTCDATE()) > 3 )    
+END    
+ELSE IF @ProjectTypeName = 'IP'    
+BEGIN    
+SELECT distinct CC.DateOfService AS DateOfService,L.Name AS ListName,CC.ClinicalCaseId AS ClinicalCaseId    
+ ,CC.PatientMRN AS PatientMRN, CC.PatientFirstName+' '+CC.PatientLastName AS Name,  ISNULL(Pro.Name,PP.Name) AS Provider       
+FROM Project p    
+JOIN ProjectType pt    
+ON p.ProjectTypeId = pt.ProjectTypeId    
+JOIN WorkItem wi    
+ON wi.ProjectId = p.ProjectId    
+JOIN ClinicalCase cc    
+ON wi.ClinicalCaseId = cc.ClinicalCaseId    
+LEFT JOIN List L ON L.ListId = CC.ListId    
+ LEFT JOIN Provider Pro ON Pro.ProviderId = CC.ProviderId   
+  LEFT JOIN WorkItemProvider Wip ON Wip.ClinicalCaseId = CC.ClinicalCaseId
+ LEFT JOIN Provider PP ON PP.ProviderID = Wip.ProviderId 
+WHERE wi.StatusId < 16 --only not posted items    
+AND pt.ProjectTypeName = @ProjectTypeName    
+AND p.ProjectId=@ProjectId    
+AND wi.StatusId = 1     
+AND (pt.ProjectTypeName = @ProjectTypeName AND DATEDIFF(DAY, cc.DateOfService, GETUTCDATE()) > 5 )    
+END    
+END    
+ELSE IF @ColumnName = 'In Coding'    
+BEGIN    
+SELECT distinct CC.DateOfService AS DateOfService,L.Name AS ListName,CC.ClinicalCaseId AS ClinicalCaseId    
+ ,CC.PatientMRN AS PatientMRN, CC.PatientFirstName+' '+CC.PatientLastName AS Name,  ISNULL(Pro.Name,PP.Name) AS Provider       
+FROM Project p    
+JOIN ProjectType pt    
+ON p.ProjectTypeId = pt.ProjectTypeId    
+JOIN WorkItem wi    
+ON wi.ProjectId = p.ProjectId    
+JOIN ClinicalCase cc    
+ON wi.ClinicalCaseId = cc.ClinicalCaseId    
+LEFT JOIN List L ON L.ListId = CC.ListId    
+ LEFT JOIN Provider Pro ON Pro.ProviderId = CC.ProviderId   
+  LEFT JOIN WorkItemProvider Wip ON Wip.ClinicalCaseId = CC.ClinicalCaseId
+ LEFT JOIN Provider PP ON PP.ProviderID = Wip.ProviderId 
+WHERE wi.StatusId < 16 --only not posted items    
+AND pt.ProjectTypeName = @ProjectTypeName    
+AND p.ProjectId=@ProjectId    
+AND wi.StatusId IN (2, 14) AND wi.isBlocked = 0    
+END    
+ELSE IF @ColumnName = 'In QA'    
+BEGIN    
+SELECT distinct CC.DateOfService AS DateOfService,L.Name AS ListName,CC.ClinicalCaseId AS ClinicalCaseId    
+ ,CC.PatientMRN AS PatientMRN, CC.PatientFirstName+' '+CC.PatientLastName AS Name,ISNULL(Pro.Name,PP.Name) AS Provider       
+FROM Project p    
+JOIN ProjectType pt    
+ON p.ProjectTypeId = pt.ProjectTypeId    
+JOIN WorkItem wi    
+ON wi.ProjectId = p.ProjectId    
+JOIN ClinicalCase cc    
+ON wi.ClinicalCaseId = cc.ClinicalCaseId    
+LEFT JOIN List L ON L.ListId = CC.ListId    
+ LEFT JOIN Provider Pro ON Pro.ProviderId = CC.ProviderId    
+  LEFT JOIN WorkItemProvider Wip ON Wip.ClinicalCaseId = CC.ClinicalCaseId
+ LEFT JOIN Provider PP ON PP.ProviderID = Wip.ProviderId
+WHERE wi.StatusId < 16 --only not posted items    
+AND pt.ProjectTypeName = @ProjectTypeName    
+AND p.ProjectId=@ProjectId    
+AND wi.StatusId IN (4, 5, 12) AND wi.isBlocked = 0    
+END    
+ELSE IF @ColumnName = 'In ShadowQA'    
+BEGIN    
+SELECT distinct CC.DateOfService AS DateOfService,L.Name AS ListName,CC.ClinicalCaseId AS ClinicalCaseId    
+ ,CC.PatientMRN AS PatientMRN, CC.PatientFirstName+' '+CC.PatientLastName AS Name,  ISNULL(Pro.Name,PP.Name) AS Provider       
+FROM Project p    
+JOIN ProjectType pt    
+ON p.ProjectTypeId = pt.ProjectTypeId    
+JOIN WorkItem wi    
+ON wi.ProjectId = p.ProjectId    
+JOIN ClinicalCase cc    
+ON wi.ClinicalCaseId = cc.ClinicalCaseId    
+LEFT JOIN List L ON L.ListId = CC.ListId    
+ LEFT JOIN Provider Pro ON Pro.ProviderId = CC.ProviderId   
+  LEFT JOIN WorkItemProvider Wip ON Wip.ClinicalCaseId = CC.ClinicalCaseId
+ LEFT JOIN Provider PP ON PP.ProviderID = Wip.ProviderId 
+WHERE wi.StatusId < 16 --only not posted items    
+AND pt.ProjectTypeName = @ProjectTypeName    
+AND p.ProjectId=@ProjectId    
+AND wi.StatusId IN (8, 9, 13) AND wi.isBlocked = 0    
+END    
+ELSE IF @ColumnName = 'Ready For Posting'    
+BEGIN    
+SELECT distinct CC.DateOfService AS DateOfService,L.Name AS ListName,CC.ClinicalCaseId AS ClinicalCaseId    
+ ,CC.PatientMRN AS PatientMRN, CC.PatientFirstName+' '+CC.PatientLastName AS Name,  
+  ISNULL(Pro.Name,PP.Name) AS Provider   
+FROM Project p    
+JOIN ProjectType pt    
+ON p.ProjectTypeId = pt.ProjectTypeId    
+JOIN WorkItem wi    
+ON wi.ProjectId = p.ProjectId    
+JOIN ClinicalCase cc    
+ON wi.ClinicalCaseId = cc.ClinicalCaseId    
+LEFT JOIN List L ON L.ListId = CC.ListId    
+ LEFT JOIN Provider Pro ON Pro.ProviderId = CC.ProviderId    
+  LEFT JOIN WorkItemProvider Wip ON Wip.ClinicalCaseId = CC.ClinicalCaseId
+ LEFT JOIN Provider PP ON PP.ProviderID = Wip.ProviderId
+WHERE wi.StatusId < 16 --only not posted items    
+AND pt.ProjectTypeName = @ProjectTypeName    
+AND p.ProjectId=@ProjectId    
+AND wi.StatusId = 15 AND wi.isBlocked = 0    
+END    
+END 
 GO
-
-
