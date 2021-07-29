0ce0073379e76a290d637d0a1f1455260dc24941
Balasiddhachary Padakanti <balasiddhachary.padakanti@aviacode.com>
2021-07-29 11:48:01 +0530

    Added code

---
+++ BuildandDeploy/TeamCityTestingDeploy.ps1
@@ -0,0 +1,14 @@
+param( [int] $deployNum, [int] $buildNum)
+
+$destComputers = @("VD-TSTPC10-WEB") # Testing Environment Server (Web Server), Hosted the ProCoder Reporting application in this server. 
+$retentionPeriod = New-TimeSpan -Days 2
+$retentionCount = 5
+
+#normally set in base file
+# Be sure to include the trailing slash on the following paths
+$webPath = "W:\Websites\UAB1\"
+$siteAndVDir = "UAB1/"
+$logPath = "W:\Logs\UAB1\"
+
+.\TeamCityDeployBase.ps1
+exit $lastexitcode
