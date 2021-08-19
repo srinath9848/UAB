c541eea0bd8b296d7b0fd3cf1fa8f6e3d94a2929
naresh.darbar <naresh.darbar@AZMLAP026>
2021-08-19 12:05:45 +0530

    UAB-2488 Unable to update Provider , allow these  operator space , () ""

--- UAB.DAL/Models/Provider.cs
+++ UAB.DAL/Models/Provider.cs
@@ -7,7 +7,7 @@ namespace UAB.DAL.Models
     {
         public int ProviderId { get; set; }
         [Required(ErrorMessage = "Provider is required.")]
-        [RegularExpression(@"^(([A-za-z0-9.() ]+[\s]{1}[A-za-z0-9.() ]+)|([A-Za-z0-9.() ]+))$", ErrorMessage = "Special Character not allowed")]
+        [RegularExpression(@"^(([A-za-z0-9 ]+[\s]{1}[A-za-z0-9 ]+)|([A-Za-z0-9 ]+))$", ErrorMessage = "Special Character not allowed")]
         public string Name { get; set; }
         public Boolean IsAuditNeeded  { get; set; }  
     }
