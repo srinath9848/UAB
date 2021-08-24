cca40444f7c7ea98ec575b6134d2c4b1edb4d7cc
Rahul Kumar Banergee <rahulkumar.banerjee@aviacode.com>
2021-08-24 09:27:08 +0530

    UAB-2314 : Fixed the issue regarding Posted charts detailed report for per month.

--- UAB.DAL/Scripts/Stored Procedures/UspPostedChartReport.sql
+++ UAB.DAL/Scripts/Stored Procedures/UspPostedChartReport.sql
@@ -1,65 +1,67 @@
-ALTER PROCEDURE [dbo].[UspPostedChartReport] --NULL,'PerDay'  
-@ProjectId INT = NULL,  
-@RangeType VARCHAR(100),
-@StartDate Date,
-@EndDate Date
-AS  
-BEGIN  
-  
-  
-IF @RangeType = 'PerDay'  
-BEGIN  
- SELECT   
- CONVERT(VARCHAR, VersionDate, 1) AS [Date]  
- , COUNT(*) AS Total  
- , COUNT(*)*100/SUM(COUNT(*)) OVER() AS [Percentage]  
- FROM WorkItem W   
- INNER JOIN Clinicalcase CC ON W.ClinicalcaseId = CC.ClinicalcaseId  
- INNER JOIN Version v ON v.ClinicalCaseId = CC.ClinicalcaseId  
- WHERE (W.ProjectId = @ProjectId OR (W.ProjectID = ISNULL(@ProjectId,W.ProjectId)))  
- AND v.StatusId in (16,17)
- AND (VersionDate >= @StartDate OR (VersionDate = ISNULL(@StartDate,VersionDate)))
- AND (VersionDate <= @EndDate OR (VersionDate = ISNULL(@EndDate,VersionDate)))
- GROUP BY CONVERT(VARCHAR, VersionDate, 1)  
-END  
-ELSE IF @RangeType = 'PerWeek'  
-BEGIN  
- SELECT DATEPART(ISO_WEEK, VersionDate) AS [Week Number],  
- DATENAME(month, VersionDate) AS [Month Name],  
- DATEPART(yyyy, VersionDate) AS [Year],  
- COUNT(*) AS Total,  
- COUNT(*)*100/SUM(COUNT(*)) OVER() AS [Percentage]  
- FROM WorkItem W   
- INNER JOIN Clinicalcase CC ON W.ClinicalcaseId = CC.ClinicalcaseId  
- INNER JOIN Version v ON v.ClinicalCaseId = CC.ClinicalcaseId  
- WHERE (W.ProjectId = @ProjectId OR (W.ProjectID = ISNULL(@ProjectId,W.ProjectId)))  
- AND v.StatusId in (16,17)
- AND (VersionDate >= @StartDate OR (VersionDate = ISNULL(@StartDate,VersionDate)))
- AND (VersionDate <= @EndDate OR (VersionDate = ISNULL(@EndDate,VersionDate)))
- GROUP BY DATEPART(ISO_WEEK, VersionDate),  
-    DATENAME(month, VersionDate),  
-    DATEPART(yyyy , VersionDate)--,DATEPART(WEEK, DATEADD(MM, DATEDIFF(MM,0,VersionDate), 0))  
-END  
-ELSE IF @RangeType = 'PerMonth'  
-BEGIN   
- SELECT DATENAME(month, VersionDate) AS [Month Name],  
- --DATEPART(mm, VersionDate) AS [Month Number],  
- DATEPART(yyyy, VersionDate) AS [Year],  
- COUNT(*) AS Total,  
- COUNT(*)*100/SUM(COUNT(*)) OVER() AS [Percentage]  
- FROM WorkItem W   
- INNER JOIN Clinicalcase CC ON W.ClinicalcaseId = CC.ClinicalcaseId  
- INNER JOIN Version v ON v.ClinicalCaseId = CC.ClinicalcaseId  
- WHERE (W.ProjectId = @ProjectId OR (W.ProjectID = ISNULL(@ProjectId,W.ProjectId)))  
- AND v.StatusId in (16,17)
- AND (VersionDate >= @StartDate OR (VersionDate = ISNULL(@StartDate,VersionDate)))
- AND (VersionDate <= @EndDate OR (VersionDate = ISNULL(@EndDate,VersionDate)))
- GROUP BY DATENAME(month, VersionDate),  
-    DATEPART(yyyy , VersionDate),  
-    DATEPART(mm, VersionDate)  
- ORDER BY DATEPART(mm, VersionDate)  
-END  
-  
-END  
-  
+ALTER PROCEDURE [dbo].[UspPostedChartReport] --NULL,'PerDay'      
+@ProjectId INT = NULL,      
+@RangeType VARCHAR(100),    
+@StartDate Datetime,    
+@EndDate Datetime,  
+@TimeZoneOffset DECIMAL(8,2)     
+AS      
+BEGIN      
+      
+      
+IF @RangeType = 'PerDay'      
+BEGIN      
+ SELECT    distinct   
+ CONVERT(VARCHAR, DATEADD(Second, @TimeZoneOffset * 60 * 60, VersionDate), 1) AS [Date]      
+ , COUNT(*) AS Total      
+ , COUNT(*)*100/SUM(COUNT(*)) OVER() AS [Percentage]      
+ FROM WorkItem W       
+ INNER JOIN Clinicalcase CC ON W.ClinicalcaseId = CC.ClinicalcaseId      
+ INNER JOIN Version v ON v.ClinicalCaseId = CC.ClinicalcaseId      
+ WHERE W.ProjectId = @ProjectId     
+ AND v.StatusId in (16)    
+ AND DATEADD(Second, @TimeZoneOffset * 60 * 60, VersionDate) >= @StartDate  
+ AND DATEADD(Second, @TimeZoneOffset * 60 * 60, VersionDate) <= @EndDate  
+ GROUP BY CONVERT(VARCHAR, DATEADD(Second, @TimeZoneOffset * 60 * 60, VersionDate), 1)     
+END      
+ELSE IF @RangeType = 'PerWeek'      
+BEGIN      
+ SELECT distinct DATEPART(ISO_WEEK, DATEADD(Second, @TimeZoneOffset * 60 * 60, VersionDate)) AS [Week Number],      
+ DATENAME(month, DATEADD(Second, @TimeZoneOffset * 60 * 60, VersionDate)) AS [Month Name],      
+ DATEPART(yyyy, DATEADD(Second, @TimeZoneOffset * 60 * 60, VersionDate)) AS [Year],      
+ COUNT(*) AS Total,      
+ COUNT(*)*100/SUM(COUNT(*)) OVER() AS [Percentage]      
+ FROM WorkItem W       
+ INNER JOIN Clinicalcase CC ON W.ClinicalcaseId = CC.ClinicalcaseId      
+ INNER JOIN Version v ON v.ClinicalCaseId = CC.ClinicalcaseId      
+ WHERE (W.ProjectId = @ProjectId OR (W.ProjectID = ISNULL(@ProjectId,W.ProjectId)))      
+ AND v.StatusId in (16)    
+ AND DATEADD(Second, @TimeZoneOffset * 60 * 60, VersionDate) >= @StartDate  
+ AND DATEADD(Second, @TimeZoneOffset * 60 * 60, VersionDate) <= @EndDate    
+ GROUP BY DATEPART(ISO_WEEK, DATEADD(Second, @TimeZoneOffset * 60 * 60, VersionDate)),      
+   DATENAME(month, DATEADD(Second, @TimeZoneOffset * 60 * 60, VersionDate)),      
+ DATEPART(yyyy, DATEADD(Second, @TimeZoneOffset * 60 * 60, VersionDate))     
+END      
+ELSE IF @RangeType = 'PerMonth'      
+BEGIN       
+ SELECT distinct 
+ DATEPART(mm, DATEADD(Second, @TimeZoneOffset * 60 * 60, VersionDate)) AS [Month Number],
+ DATENAME(month, DATEADD(Second, @TimeZoneOffset * 60 * 60, VersionDate)) AS [Month Name],      
+ --DATEPART(mm, VersionDate) AS [Month Number],      
+ DATEPART(yyyy, DATEADD(Second, @TimeZoneOffset * 60 * 60, VersionDate)) AS [Year],      
+ COUNT(*) AS Total,      
+ COUNT(*)*100/SUM(COUNT(*)) OVER() AS [Percentage]      
+ FROM WorkItem W       
+ INNER JOIN Clinicalcase CC ON W.ClinicalcaseId = CC.ClinicalcaseId      
+ INNER JOIN Version v ON v.ClinicalCaseId = CC.ClinicalcaseId      
+ WHERE (W.ProjectId = @ProjectId OR (W.ProjectID = ISNULL(@ProjectId,W.ProjectId)))      
+ AND v.StatusId in (16)    
+ AND DATEADD(Second, @TimeZoneOffset * 60 * 60, VersionDate) >= @StartDate  
+ AND DATEADD(Second, @TimeZoneOffset * 60 * 60, VersionDate) <= @EndDate  
+ GROUP BY DATENAME(month, DATEADD(Second, @TimeZoneOffset * 60 * 60, VersionDate)),      
+    DATEPART(yyyy , DATEADD(Second, @TimeZoneOffset * 60 * 60, VersionDate)),      
+    DATEPART(mm, DATEADD(Second, @TimeZoneOffset * 60 * 60, VersionDate))      
+ ORDER BY DATEPART(mm, DATEADD(Second, @TimeZoneOffset * 60 * 60, VersionDate))      
+END      
+      
+END      
 GO
