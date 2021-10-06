91bca5c51c7e8e8ea7b5dbed3668e7b6f4f7ea27
Rahul Kumar Banergee <rahulkumar.banerjee@aviacode.com>
2021-10-06 16:33:56 +0530

    UAB-2797 : Fixed button alignment and added mandatory criteria for Type.

--- UAB.DAL/Models/BlockCategory.cs
+++ UAB.DAL/Models/BlockCategory.cs
@@ -11,7 +11,7 @@ namespace UAB.DAL.Models
         [Required(ErrorMessage = "Block Category is required.")]
         [RegularExpression(@"^(([A-za-z0-9 ]+[\s]{1}[A-za-z0-9 ]+)|([A-Za-z0-9 ]+))$", ErrorMessage = "Special Character not allowed")]
         public string Name { get; set; }
-
+        [Required(ErrorMessage = "Block Type is required.")]
         public string BlockType { get; set; } 
     }
 }
